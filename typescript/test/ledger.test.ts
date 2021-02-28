7fe1037fd54f17537f38b0b1d9c368bc31fbffe1
gjgd <gjgd@protonmail.com>
2021-02-28 18:25:25 +0000

    chore: add typescript folder

--- typescript/test/ledger.test.ts
+++ typescript/test/ledger.test.ts
@@ -2,6 +2,5 @@ import { getLedger } from '../src';
 
 it('should not have typings errors', async () => {
   const ledger = await getLedger();
-  console.log(ledger);
   expect(ledger).toBeDefined();
 });
